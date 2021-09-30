07a9b84df47eb9e37593d38752c24c3cf1e268ba
xuchenhao001 <xu.chenhao@hotmail.com>
2021-09-30 14:24:12 +1000

    adjust server learning rate to 0.0001

--- federated-learning/utils/options.py
+++ federated-learning/utils/options.py
@@ -11,7 +11,7 @@ def args_parser():
     parser.add_argument('--local_bs', type=int, default=10, help="local batch size: B")
     parser.add_argument('--bs', type=int, default=128, help="test batch size")
     parser.add_argument('--lr', type=float, default=0.01, help="learning rate")
-    parser.add_argument('--server_lr', type=float, default=0.01, help="server learning rate")
+    parser.add_argument('--server_lr', type=float, default=0.0001, help="server learning rate")
     parser.add_argument('--momentum', type=float, default=0.5, help="SGD momentum (default: 0.5)")
     parser.add_argument('--split', type=str, default='user', help="train-test split type, user or sample")
 
