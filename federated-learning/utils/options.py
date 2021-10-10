119746d64b38a54f76a78c9e464143dad95ef988
xuchenhao001 <xu.chenhao@hotmail.com>
2021-10-10 22:20:26 +1100

    increase local batch size from 10 to 64

--- federated-learning/utils/options.py
+++ federated-learning/utils/options.py
@@ -8,7 +8,7 @@ def args_parser():
     # parser.add_argument('--num_users', type=int, default=15, help="number of users: K")
     parser.add_argument('--frac', type=float, default=1.0, help="the fraction of clients: C")
     parser.add_argument('--local_ep', type=int, default=5, help="the number of local epochs: E")
-    parser.add_argument('--local_bs', type=int, default=10, help="local batch size: B")
+    parser.add_argument('--local_bs', type=int, default=64, help="local batch size: B")
     parser.add_argument('--bs', type=int, default=128, help="test batch size")
     parser.add_argument('--lr', type=float, default=0.01, help="learning rate")
     parser.add_argument('--momentum', type=float, default=0.5, help="SGD momentum (default: 0.5)")
