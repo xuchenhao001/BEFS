9a9395c655f20e311e9232b61705faf2ac75b3e8
xuchenhao001 <xu.chenhao@hotmail.com>
2021-11-02 12:32:41 +1100

    result: comparison of the gradient size

--- federated-learning/plot/utils/time_acc_base.py
+++ federated-learning/plot/utils/time_acc_base.py
@@ -72,152 +72,44 @@ def plot_time_acc(title, fed_sync_sgd, fed_sync, fed_efsign, fed_avg, local_trai
         plt.show()
 
 
-def plot_time_acc_lrsc(title, fed_sync_lrsc_5, fed_sync_lrsc_10, fed_sync_lrsc_20, fed_sync, fed_avg, save_path=None, plot_size="L"):
+def plot_time_bar(title, sgd, sign_sgd, save_path=None, plot_size="L"):
     font_settings = get_font_settings(plot_size)
-    x = range(1, len(fed_sync_lrsc_5) + 1)
+    x = ["CNN-CIFAR10", "CNN-FMNIST", "MLP-FMNIST"]
 
-    fig, axes = plt.subplots()
-
-    axes.plot(x, fed_sync_lrsc_5, label="Scale Round=5", linewidth=1)
-    axes.plot(x, fed_sync_lrsc_10, label="Scale Round=10", linewidth=1)
-    axes.plot(x, fed_sync_lrsc_20, label="Scale Round=20", linewidth=1)
-    axes.plot(x, fed_sync, label="BEFL", linestyle='--', alpha=0.5)
-    axes.plot(x, fed_avg, label="FedAVG", linestyle='--', alpha=0.5)
-
-    axes.set_xlabel("Training Round", **font_settings.get("cs_xy_label_font"))
-    axes.set_ylabel("Average Test Accuracy (%)", **font_settings.get("cs_xy_label_font"))
-
-    plt.title(title, **font_settings.get("cs_title_font"))
-    plt.xticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.yticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.tight_layout()
-    # plt.xlim(0, xrange)
-    plt.legend(prop=font_settings.get("legend_font"), loc='lower right')
-    plt.grid()
-    if save_path:
-        plt.savefig(save_path)
-    else:
-        plt.show()
-
-
-
-def plot_static_time_acc(title, scale, xrange, fed_async, fed_async_f05, fed_async_f10, fed_async_f15, save_path=None,
-                         is_acc=True, plot_size="L"):
-    font_settings = get_font_settings(plot_size)
-    x = range(len(fed_async))
-    x = [value * scale for value in x]
-
-    fig, axes = plt.subplots()
-
-    axes.plot(x, fed_async, label="Dynamic", linewidth=3)
-    axes.plot(x, fed_async_f05, label=r'$\mathbf{\epsilon}$ = 0.5', linestyle='--', alpha=0.5)
-    axes.plot(x, fed_async_f10, label=r'$\epsilon$ = 1.0', linestyle='--', alpha=0.5)
-    axes.plot(x, fed_async_f15, label=r'$\epsilon$ = 1.5', linestyle='--', alpha=0.5)
-
-    axes.set_xlabel("Running Time (seconds)", **font_settings.get("cs_xy_label_font"))
-    if is_acc:
-        axes.set_ylabel("Average Test Accuracy (%)", **font_settings.get("cs_xy_label_font"))
-    else:
-        axes.set_ylabel("Average Loss (MSE)", **font_settings.get("cs_xy_label_font"))
-
-    plt.title(title, **font_settings.get("cs_title_font"))
-    plt.xticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.yticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.tight_layout()
-    plt.xlim(0, xrange)
-    plt.legend(prop=font_settings.get("legend_font"), loc='lower right')
-    plt.grid()
-    if save_path:
-        plt.savefig(save_path)
-    else:
-        plt.show()
-
-
-def plot_time_cost(title, yrange, fed_async, fed_avg, fed_sync, fed_localA, local_train, save_path=None, plot_size="L"):
-    font_settings = get_font_settings(plot_size)
-    x = range(1, len(fed_async)+1)
-
-    fig, axes = plt.subplots()
-
-    axes.plot(x, fed_async, label="BAFL", linewidth=3)
-    axes.plot(x, fed_sync, label="BSFL", linestyle='--', alpha=0.5)
-    axes.plot(x, fed_localA, label="APFL", linestyle='--', alpha=0.5)
-    axes.plot(x, fed_avg, label="FedAVG", linestyle='--', alpha=0.5)
-    axes.plot(x, local_train, label="Local", linestyle='--', alpha=0.5)
-
-    axes.set_xlabel("Training Round", **font_settings.get("cs_xy_label_font"))
-    axes.set_ylabel("Average Time (s)", **font_settings.get("cs_xy_label_font"))
-
-    plt.title(title, **font_settings.get("cs_title_font"))
-    plt.xticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.yticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.tight_layout()
-    plt.ylim(0, yrange)
-    plt.legend(prop=font_settings.get("legend_font"), loc='upper right')
-    plt.grid()
-    if save_path:
-        plt.savefig(save_path)
-    else:
-        plt.show()
-
-
-def plot_time_historgram(title, fed_async, fed_avg, fed_sync, fed_localA, local_train, save_path=None, plot_size="L"):
-    font_settings = get_font_settings(plot_size)
-    x = ["Train", "Test", "Communication", "Round"]
-
-    fig, axes = plt.subplots()
+    # fig, axes = plt.subplots()
+    fig, axes = plt.subplots(1, 3, tight_layout=True)
 
     width = 0.15  # the width of the bars
-    axes.bar([(p - width * 2) for p in range(len(x))], height=fed_async, width=width, label="BAFL", hatch='x')
-    axes.bar([p - width for p in range(len(x))], height=fed_sync, width=width, label="BSFL", hatch='o')
-    axes.bar(range(len(x)), height=fed_localA, width=width, label="APFL", hatch='+')
-    axes.bar([p + width for p in range(len(x))], height=fed_avg, width=width, label="FedAVG", hatch='*')
-    axes.bar([(p + width * 2) for p in range(len(x))], height=local_train, width=width, label="Local",
-             hatch='/')
+    axes[0].bar(0 - width / 2, height=sgd[0], width=width, label="SGD",
+                hatch='x')
+    axes[0].bar(0 + width / 2, height=sign_sgd[0], width=width, label="SignSGD", hatch='*')
 
-    plt.xticks(range(len(x)), x)
-    axes.set_xlabel("Steps in the Training Round", **font_settings.get("cs_xy_label_font"))
-    axes.set_ylabel("Average Time (s)", **font_settings.get("cs_xy_label_font"))
+    axes[1].bar(0 - width / 2, height=sgd[1], width=width, label="SGD",
+                hatch='x')
+    axes[1].bar(0 + width / 2, height=sign_sgd[1], width=width, label="SignSGD", hatch='*')
 
-    plt.title(title, **font_settings.get("cs_title_font"))
-    plt.xticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.yticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.tight_layout()
-    plt.legend(prop=font_settings.get("legend_font"), loc='upper left')
-    plt.grid()
-    if save_path:
-        plt.savefig(save_path)
-    else:
-        plt.show()
+    axes[2].bar(0 - width / 2, height=sgd[2], width=width, label="SGD",
+                hatch='x')
+    axes[2].bar(0 + width / 2, height=sign_sgd[2], width=width, label="SignSGD", hatch='*')
 
+    for i in range(len(axes)):
+        # Use the pyplot interface to change just one subplot
+        plt.sca(axes[i])
+        plt.grid()
+        plt.xticks([0], [x[i]], **font_settings.get("cs_xy_ticks_font"))
+        plt.yticks(**font_settings.get("cs_xy_ticks_font"))
 
-def plot_poisoning_time_acc(title, scale, xrange, acc_average, acc_node_list, save_path=None, is_acc=True, plot_size="L"):
-    font_settings = get_font_settings(plot_size)
-    x = range(len(acc_average))
-    x = [value * scale for value in x]
+    fig.supxlabel("The Type of Model and Datasets", **font_settings.get("cs_xy_label_font"))
+    fig.supylabel("The Size of Gradients (MB)", **font_settings.get("cs_xy_label_font"))
+    fig.suptitle(title, **font_settings.get("cs_title_font"))
 
-    fig, axes = plt.subplots()
+    plt.subplots_adjust(top=1.9)
+    handles, labels = axes[2].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', prop=font_settings.get("legend_font"), ncol=2)
+    # fig.legend(handles, labels, prop=font_settings.get("legend_font"), ncol=2, bbox_to_anchor=(1.0, 1.05))
+    # fig.legend(handles, labels, loc='upper right', prop=font_settings.get("legend_font"))
 
-    axes.plot(x, acc_average, label="Average", linewidth=3)
-    axes.plot(x, acc_node_list[0], label="Node 1", linestyle='--', alpha=0.5)
-    axes.plot(x, acc_node_list[1], label="Node 2", linestyle='--', alpha=0.5)
-    axes.plot(x, acc_node_list[2], label="Node 3", linestyle='--', alpha=0.5)
-    axes.plot(x, acc_node_list[3], label="Node 4", linestyle='--', alpha=0.5)
-    axes.plot(x, acc_node_list[4], label="Node 5", linestyle='--', alpha=0.5)
-
-    axes.set_xlabel("Running Time (seconds)", **font_settings.get("cs_xy_label_font"))
-    if is_acc:
-        axes.set_ylabel("Test Accuracy (%)", **font_settings.get("cs_xy_label_font"))
-    else:
-        axes.set_ylabel("Average Loss (MSE)", **font_settings.get("cs_xy_label_font"))
-
-    plt.title(title, **font_settings.get("cs_title_font"))
-    plt.xticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.yticks(**font_settings.get("cs_xy_ticks_font"))
-    plt.tight_layout()
-    plt.xlim(0, xrange)
-    plt.legend(prop=font_settings.get("legend_font"), loc='lower right')
-    plt.grid()
+    fig.tight_layout()
     if save_path:
         plt.savefig(save_path)
     else:
