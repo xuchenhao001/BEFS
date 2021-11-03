27f6d454084d8d63595ca107dbbc23425fbe8193
xuchenhao001 <xu.chenhao@hotmail.com>
2021-11-03 16:53:42 +1100

    auto test script for ddos 99% and results of ddos 90% and 95%

--- federated-learning/plot/utils/time_acc_base.py
+++ federated-learning/plot/utils/time_acc_base.py
@@ -116,29 +116,26 @@ def plot_time_bar(title, sgd, sign_sgd, save_path=None, plot_size="L"):
         plt.show()
 
 
-def plot_ddos_acc(title, scale, xrange, ddos_00, ddos_80, ddos_90, save_path=None, is_acc=True, plot_size="L"):
+def plot_ddos_acc(title, fed_sync_sgd, fed_avg, fed_efsign, fed_sign, save_path=None, is_acc=True, plot_size="L"):
     font_settings = get_font_settings(plot_size)
-    x = range(len(ddos_00))
-    x = [value * scale for value in x]
+    x = range(1, len(fed_sync_sgd) + 1)
 
     fig, axes = plt.subplots()
 
-    axes.plot(x, ddos_00, label="Normal")
-    axes.plot(x, ddos_80, label="DDoS 80%")
-    axes.plot(x, ddos_90, label="DDoS 90%")
+    axes.plot(x, fed_sync_sgd, label="BEFS-signSGD", linewidth=3, zorder=10)
+    axes.plot(x, fed_efsign, label="EF-signSGD")
+    axes.plot(x, fed_sign, label="signSGD")
+    axes.plot(x, fed_avg, label="FedAVG")
 
-    axes.set_xlabel("Running Time (seconds)", **font_settings.get("cs_xy_label_font"))
-    if is_acc:
-        axes.set_ylabel("Average Test Accuracy (%)", **font_settings.get("cs_xy_label_font"))
-    else:
-        axes.set_ylabel("Average Loss (MSE)", **font_settings.get("cs_xy_label_font"))
+    axes.set_xlabel("Training Round", **font_settings.get("cs_xy_label_font"))
+    axes.set_ylabel("Average Test Accuracy (%)", **font_settings.get("cs_xy_label_font"))
 
     plt.title(title, **font_settings.get("cs_title_font"))
     plt.xticks(**font_settings.get("cs_xy_ticks_font"))
     plt.yticks(**font_settings.get("cs_xy_ticks_font"))
     plt.tight_layout()
-    plt.xlim(0, xrange)
-    plt.legend(prop=font_settings.get("legend_font"), loc='lower right')
+    # plt.xlim(0, xrange)
+    plt.legend(prop=font_settings.get("legend_font"), loc='lower right').set_zorder(11)
     plt.grid()
     if save_path:
         plt.savefig(save_path)
